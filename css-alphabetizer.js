c2b9b42c0d377a853c16f4c393cdd201dc5cc26b
GeneralKenobi1 <43397938+GeneralKenobi1@users.noreply.github.com>
2020-07-27 08:51:50 -0700

    block->rule, parameter->declaration

--- css-alphabetizer.js
+++ css-alphabetizer.js
@@ -3,87 +3,96 @@
 
 "use strict";
 
+/*
+CSS - This whole thing is a rule: 
+
+selector {
+	declaration;
+	declaration;
+}
+*/
+
 class CSSAlphabetizer {
 	alphabetize(s) {
-		let lines, blocks;
+		let lines, rules;
 		
 		// TODO: this._validate();
 		// TODO: this._beautify(); using regex and replace of { ; }
 		
 		lines = this._makeLinesArray(s);
-		blocks = this._sortSelectors(lines);
-		blocks = this._sortParameters(blocks);
+		rules = this._sortSelectors(lines);
+		rules = this._sortDeclarations(rules);
 		
-		s = this._makeString(blocks);
-		s = this._addBlankLineBetweenBlocks(s);
+		s = this._makeString(rules);
+		s = this._addBlankLineBetweenRules(s);
 		s = s.trim();
 		
 		return s;
 	}
 	
-	_addBlankLineBetweenBlocks(s) {
+	_addBlankLineBetweenRules(s) {
 		// Replace } enter with } enter enter
 		return s.replace(/}\r?\n(?=[^\r\n])/, "}\n\n");
 	}
 	
-	_makeString(blocks) {
-		// blocks => lines
-		const lines = blocks.map(block => block[1]).flat(); 
+	_makeString(rules) {
+		// rules => lines
+		const lines = rules.map(rule => rule[1]).flat(); 
 		
 		// lines => string
 		return lines.map(line=>line[1]).join("\n");
 	}
 	
 	_sortSelectors(lines) {
-		/** [parameterTrimmed, [line, line, ...line]] */
-		let blocks = [];
+		/** [declarationTrimmed, [line, line, ...line]] */
+		let rules = [];
 		/** [line, line, ...line] **/
 		let buffer = [];
 		let lineNumTracker = 0;
-		let parameterTrimmed = "";
+		let declarationTrimmed = "";
 		let len = lines.length;
 		
 		for (let i = 0; i < len; i++) {
 			let line = lines[i];
 			let lineNum = line[2];
 			
-			if (! parameterTrimmed && line[0].includes("{")) {
-				parameterTrimmed = line[0];
+			if (! declarationTrimmed && line[0].includes("{")) {
+				declarationTrimmed = line[0];
 			}
 			
 			if (lineNum !== lineNumTracker) {
 				lineNumTracker++;
-				blocks.push([parameterTrimmed, buffer]);
+				rules.push([declarationTrimmed, buffer]);
 				buffer = [];
-				parameterTrimmed = "";
+				declarationTrimmed = "";
 			}
 			
 			buffer.push(line);
 			
-			// Last line. Finish the block.
+			// Last line. Finish the rule.
 			if (i === len-1) {
 				lineNumTracker++;
-				blocks.push([parameterTrimmed, buffer]);
+				rules.push([declarationTrimmed, buffer]);
 				buffer = [];
-				parameterTrimmed = "";
+				declarationTrimmed = "";
 			}
 		}
 		
-		blocks = blocks.sort();
+		rules = rules.sort();
 		
-		return blocks;
+		return rules;
 	}
 	
-	_sortParameters(blocks) {
-		for (let key in blocks) {
-			let lines = blocks[key][1];
+	_sortDeclarations(rules) {
+		for (let key in rules) {
+			let lines = rules[key][1];
 			let lineBuffer = [];
 			let sortBuffer = [];
 			
 			for (let line of lines) {
-				let isParameter = line[3];
+				let isDeclaration = line[3];
 				
-				if (isParameter) {
+				if (isDeclaration) {
 					sortBuffer.push(line);
 				} else {
 					// If last line
@@ -96,41 +105,41 @@ class CSSAlphabetizer {
 				}
 			}
 			
-			blocks[key][1] = lineBuffer;
+			rules[key][1] = lineBuffer;
 		}
-		console.log(blocks);
-		return blocks;
+		console.log(rules);
+		return rules;
 	}
 	
-	/** Makes an array of lines. Outer level [line, line, ...line]. Inner level [string trimmed, string notTrimmed, int blockNum, bool isParameter]. Having unique block numbers will be important later, when we are alphabetizing things. */
+	/** Makes an array of lines. Outer level [line, line, ...line]. Inner level [string trimmed, string notTrimmed, int ruleNum, bool isDeclaration]. Having unique rule numbers will be important later, when we are alphabetizing things. */
 	_makeLinesArray(s) {
 		// TODO: refactor to use associative array, more readable code, and less likely to have bugs if later we want to add/change/delete fields
 		let lines = s.split("\n");
-		let blockNum = 0;
-		let isParameter = false;
+		let ruleNum = 0;
+		let isDeclaration = false;
 		
 		for (let key in lines) {
 			let value = lines[key];
 			
 			if (value.includes("}")) {
-				isParameter = false;
+				isDeclaration = false;
 			}
 			
 			lines[key] = [
 				// Trimmed line in first spot. Important for sorting.
 				value.trim(),
 				value,
-				blockNum,
-				isParameter,
+				ruleNum,
+				isDeclaration,
 			];
 			
-			// When } is found, increment the block number. This keeps comment lines above block grouped with that block.
+			// When } is found, increment the rule number. This keeps comment lines above rule grouped with that rule.
 			if (value.includes("}")) {
-				blockNum++;
+				ruleNum++;
 			}
 			
 			if (value.includes("{")) {
-				isParameter = true;
+				isDeclaration = true;
 			}
 		}
 		
