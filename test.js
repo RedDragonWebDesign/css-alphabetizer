7b796e58d2c54e71ad060aa57f38ac78da93d69c
GeneralKenobi1 <43397938+GeneralKenobi1@users.noreply.github.com>
2020-08-06 09:17:54 -0700

    reorganize tests

---
+++ test.js
@@ -0,0 +1,20 @@
+let tests = [
+	'little-tests/bad-syntax',
+	'little-tests/comment-right-of-declaration',
+	'little-tests/comment-top-of-declaration',
+	'little-tests/comment-top-of-file',
+	'little-tests/comment-top-of-rule',
+	'little-tests/compact',
+	'little-tests/media-query',
+	'little-tests/media-query-nested',
+	'little-tests/multiple-selectors',
+	'little-tests/simple',
+	'little-tests/whitespace-beginning',
+	'little-tests/whitespace-end',
+];
+
+// run tests
+
+// format them in HTML
+
+// print
